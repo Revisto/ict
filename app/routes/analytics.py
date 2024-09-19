1518fcc8e6ea7450102ec5057131d9990bd80c16
Revisto <theRevisto@gmail.com>
2024-09-19 09:42:38 +0330

    fix analytics page

--- app/routes/analytics.py
+++ app/routes/analytics.py
@@ -1,5 +1,6 @@
 # app/routes/analytics.py
-from flask import Blueprint, jsonify, render_template
+from flask import Blueprint, jsonify, render_template, session
+from app.decorators import company_required
 from app.models import Campaign, CampaignGame, GameAnalytics, GeneralAnalytics, Game
 
 bp = Blueprint('analytics', __name__)
@@ -21,7 +22,9 @@ def analytics_dashboard(campaign_id, game_id):
     return render_template('analytics_dashboard.html', campaign=campaign, game=game)
 
 @bp.route('/analytics', methods=['GET'])
+@company_required
 def main_analytics_dashboard():
-    campaigns = Campaign.query.all()
+    company_id = session['company_id']
+    campaigns = Campaign.query.filter_by(company_id=company_id).all()
     campaign_games = {campaign.id: CampaignGame.query.filter_by(campaign_id=campaign.id).all() for campaign in campaigns}
     return render_template('main_analytics_dashboard.html', campaigns=campaigns, campaign_games=campaign_games)
